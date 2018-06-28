5615efdcfa9eac52ce2ea30a3565c3f77912a881
Laptop\Roger <roger.birkenes+github@gmail.com>
2018-06-28 10:37:26 +0200

    Trying to render stuff

--- strudel/strudelView.java
+++ strudel/strudelView.java
@@ -14,8 +14,6 @@ public class strudelView {
     private final int width = 360;
     private int height = 640;
     public Pane root = new Pane();
-    final Canvas canvas = new Canvas(width, height);
-    GraphicsContext gc = canvas.getGraphicsContext2D();
     
     strudelLogic logic = new strudelLogic();
     
@@ -28,30 +26,58 @@ public class strudelView {
         header.setStyle("-fx-background-color: #F01010;");
         
         root.setStyle("-fx-background-color: #FFFFFF;");
-        populateStrudels(root);
+        Pane content = new Pane();
+        content.setTranslateX(0);
+        content.setTranslateY(31);
+        content.setPrefSize(width+30, height-51);
+        content.setStyle("-fx-background-color: #FFFFFF;");
+        populateStrudels(content);
+        
+        Text test = new Text("ASDF");
+        test.setFill(Color.BLACK);
+        test.setX(20);
+        test.setY(50);
+        test.setFont(test.getFont().font(20)); 
+        test.toFront();
+        root.getChildren().add(test);
         
         Pane footer = new Pane();
         footer.setPrefWidth(width+15);
         footer.setPrefHeight(30);
         footer.setTranslateY(height-18);
         footer.setStyle("-fx-background-color: #F0591E;");
+        header.toFront();
+        footer.toFront();
         
-        root.getChildren().addAll(header, footer, canvas);
+        root.getChildren().addAll(header, footer, content);
         
         return root;
     }
     
     public Pane populateStrudels(Pane pane) {
         ArrayList<Strudel> strudels = logic.getTest();
-        int nextHeight = 30;
+        int nextHeight = 0;
         for (Strudel s : strudels) {
-            gc.setFill(Color.web(s.getColor()));
-            gc.fillRect(0, nextHeight, width+15, s.getHeight());
-            gc.setFill(Color.WHITE);
+            Pane p = new Pane();
+            p.setTranslateX(0);
+            p.setTranslateY(nextHeight);
+            p.setPrefSize(width+30, s.getHeight());
+            p.setStyle("-fx-background-color: " + s.getColor());
+            setText(pane, s, nextHeight);
+            pane.getChildren().add(p);
             
             nextHeight += s.getHeight();
-        }
+        }        
         return pane;
+    }
+    
+    public void setText(Pane pane, Strudel s, int height) {
+        Text t = new Text(s.getMessage());
+        t.setX(10);
+        System.out.println(height);
+        t.setY(height);
+        t.setFill(Color.WHITE);
+        pane.getChildren().add(t);
     }
     
     public void setHeight(int height) {
