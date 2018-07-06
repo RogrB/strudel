ed0c743c273d75d26716886f5cc361ae3b833d6f
Laptop\Roger <roger.birkenes+github@gmail.com>
2018-07-06 10:08:28 +0200

    minor io fix

--- io/io.java
+++ io/io.java
@@ -2,8 +2,17 @@ package io;
 
 public class io {
     
+    private int karma;
+    
+    public io() {
+        this.karma = 10;
+    }
     public int getKarma() {
-        return 10;
+        return this.karma;
+    }
+    
+    public void setKarma(int karma) {
+        this.karma = karma;
     }
     
 }
